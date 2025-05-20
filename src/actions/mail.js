e9596ba88bc85afa0ff93e28311a77ed4fb31d30
kevin <kevinrkowalski@gmail.com>
2025-05-20 12:01:41 -0400

    adding phone number validation error message

--- src/actions/mail.js
+++ src/actions/mail.js
@@ -6,8 +6,8 @@ import twilio from "twilio";
 
 const mailgun = new Mailgun(FormData);
 const mg = mailgun.client({
-	username: "api",
-	key: process.env.MAILGUN_API_KEY,
+  username: "api",
+  key: process.env.MAILGUN_API_KEY,
 });
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -15,117 +15,117 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
 async function verifyCaptcha(hcaptcha) {
-	const data = new URLSearchParams();
-	data.append("secret", process.env.HCAPTCHA_SECRET_KEY);
-	data.append("response", hcaptcha);
+  const data = new URLSearchParams();
+  data.append("secret", process.env.HCAPTCHA_SECRET_KEY);
+  data.append("response", hcaptcha);
 
-	const response = await fetch("https://api.hcaptcha.com/siteverify", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/x-www-form-urlencoded",
-			Accept: "application/json",
-		},
-		body: data.toString(),
-	});
+  const response = await fetch("https://api.hcaptcha.com/siteverify", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json",
+    },
+    body: data.toString(),
+  });
 
-	const responseData = await response.json();
-	return responseData;
+  const responseData = await response.json();
+  return responseData;
 }
 
 async function verifyPhoneWithTwilio(number) {
-	try {
-		const phoneNumber = await client.lookups.v2
-			.phoneNumbers(number)
-			.fetch({ fields: ["line_type_intelligence"] });
+  try {
+    const phoneNumber = await client.lookups.v2
+      .phoneNumbers(number)
+      .fetch({ fields: ["line_type_intelligence"] });
 
-		const goodLineTypes = ["mobile", "landline"];
+    const goodLineTypes = ["mobile", "landline"];
 
-		if (
-			!phoneNumber.valid ||
-			!goodLineTypes.includes(phoneNumber?.lineTypeIntelligence?.type)
-		) {
-			return false;
-		}
+    if (
+      !phoneNumber.valid ||
+      !goodLineTypes.includes(phoneNumber?.lineTypeIntelligence?.type)
+    ) {
+      return false;
+    }
 
-		return true;
-	} catch (err) {
-		// There was an error verifying the phone number but let's assume it's valid
-		console.log("error verifying phone number with twilio");
-		return true;
-	}
+    return true;
+  } catch (err) {
+    // There was an error verifying the phone number but let's assume it's valid
+    console.log("error verifying phone number with twilio");
+    return true;
+  }
 }
 
 export async function submitForm(formData) {
-	const {
-		name,
-		email,
-		phone,
-		project,
-		message,
-		formCheck: honeyPot,
-		"h-captcha-response": hcaptcha,
-	} = formData;
+  const {
+    name,
+    email,
+    phone,
+    project,
+    message,
+    formCheck: honeyPot,
+    "h-captcha-response": hcaptcha,
+  } = formData;
 
-	if (honeyPot) {
-		return {
-			success: false,
-			message: "Something went wrong, please try again later.",
-		};
-	}
+  if (honeyPot) {
+    return {
+      success: false,
+      message: "Something went wrong, please try again later.",
+    };
+  }
 
-	if (!hcaptcha) {
-		return {
-			success: false,
-			message: "Please complete the captcha challenge.",
-		};
-	}
+  if (!hcaptcha) {
+    return {
+      success: false,
+      message: "Please complete the captcha challenge.",
+    };
+  }
 
-	try {
-		const responseData = await verifyCaptcha(hcaptcha);
+  try {
+    const responseData = await verifyCaptcha(hcaptcha);
 
-		if (!responseData.success) {
-			return {
-				success: false,
-				message: "Captcha verification failed. Please try again.",
-			};
-		}
+    if (!responseData.success) {
+      return {
+        success: false,
+        message: "Captcha verification failed. Please try again.",
+      };
+    }
 
-		const isValidPhoneNumber = await verifyPhoneWithTwilio(phone);
+    const isValidPhoneNumber = await verifyPhoneWithTwilio(phone);
 
-		if (!isValidPhoneNumber) {
-			return {
-				success: false,
-				message: "Invalid phone number. Please try again.",
-			};
-		}
+    if (!isValidPhoneNumber) {
+      return {
+        success: false,
+        message: "Invalid phone number. Please try again.",
+      };
+    }
 
-		const emailResponse = await mg.messages.create(process.env.MAILGUN_DOMAIN, {
-			from: `NEKB <postmaster@${process.env.MAILGUN_DOMAIN}>`,
-			to: ["thais@northeastkitchenbath.com"],
-			subject: "New Website Form Submission",
-			text: `
-				Name: ${name}
-				Email: ${email}
-				Phone: ${phone}
-				Project: ${project}
-				Message: ${message}
-			`,
-		});
+    const emailResponse = await mg.messages.create(process.env.MAILGUN_DOMAIN, {
+      from: `NEKB <postmaster@${process.env.MAILGUN_DOMAIN}>`,
+      to: ["thais@northeastkitchenbath.com"],
+      subject: "New Website Form Submission",
+      text: `
+    		Name: ${name}
+    		Email: ${email}
+    		Phone: ${phone}
+    		Project: ${project}
+    		Message: ${message}
+    	`,
+    });
 
-		// Check the status of the email response
-		if (emailResponse.status !== 200) {
-			return {
-				success: false,
-				message: "Failed to send email. Please try again later.",
-			};
-		}
+    // Check the status of the email response
+    if (emailResponse.status !== 200) {
+      return {
+        success: false,
+        message: "Failed to send email. Please try again later.",
+      };
+    }
 
-		return { success: true, message: "Form submitted successfully!" };
-	} catch (err) {
-		return {
-			success: false,
-			message:
-				"There was an error with the captcha challenge or email sending. Please try again later.",
-		};
-	}
+    return { success: true, message: "Form submitted successfully!" };
+  } catch (err) {
+    return {
+      success: false,
+      message:
+        "There was an error with the captcha challenge or email sending. Please try again later.",
+    };
+  }
 }
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -10,262 +10,265 @@ import { usePathname } from "next/navigation";
 import { submitForm } from "@/actions/mail";
 
 const ContactForm = ({
-	title = "",
-	submitButtonClasses = "",
-	submitButtonText = "Send Message",
+  title = "",
+  submitButtonClasses = "",
+  submitButtonText = "Send Message",
 }) => {
-	const path = usePathname();
-	const {
-		register,
-		handleSubmit,
-		reset,
-		setValue,
-		setError,
-		formState: { errors, isSubmitSuccessful, isSubmitting },
-	} = useForm({
-		mode: "onTouched",
-		defaultValues: {
-			"h-captcha-response": "",
-		},
-	});
+  const path = usePathname();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setValue,
+    setError,
+    formState: { errors, isSubmitSuccessful, isSubmitting },
+  } = useForm({
+    mode: "onTouched",
+    defaultValues: {
+      "h-captcha-response": "",
+    },
+  });
 
-	// Register the captcha field with validation
-	register("h-captcha-response", {
-		required: "Please complete the captcha",
-	});
+  // Register the captcha field with validation
+  register("h-captcha-response", {
+    required: "Please complete the captcha",
+  });
 
-	const [isSuccess, setIsSuccess] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
 
-	const onHCaptchaChange = (token) => {
-		setValue("h-captcha-response", token);
-	};
+  const onHCaptchaChange = (token) => {
+    setValue("h-captcha-response", token);
+  };
 
-	const handleOnSubmit = async (formData) => {
-		const response = await submitForm(formData);
+  const handleOnSubmit = async (formData) => {
+    const response = await submitForm(formData);
+    setIsSuccess(response.success);
+    if (response.success) {
+      sendGTMEvent({
+        event: "formSubmission",
+        pagePath: path,
+        pageTitle: document.title,
+        formStatus: "success",
+      });
+      reset();
+    } else {
+      if (response.message.includes("Invalid phone number")) {
+        setError("phone", { type: "manual", message: response.message });
+      } else {
+        setError("form", { type: "manual", message: response.message });
+      }
+      sendGTMEvent({
+        event: "formSubmission",
+        pagePath: path,
+        pageTitle: document.title,
+        formStatus: "failed",
+      });
+    }
+  };
 
-		setIsSuccess(response.success);
-		if (response.success) {
-			sendGTMEvent({
-				event: "formSubmission",
-				pagePath: path,
-				pageTitle: document.title,
-				formStatus: "success",
-			});
-			reset();
-		} else {
-			setError("form", { type: "manual", message: response.message });
-			sendGTMEvent({
-				event: "formSubmission",
-				pagePath: path,
-				pageTitle: document.title,
-				formStatus: "failed",
-			});
-		}
-	};
+  return (
+    <div className="p-8 bg-white">
+      {title && (
+        <Heading level="span" levelStyle="h4" className="mb-4">
+          {title}
+        </Heading>
+      )}
+      <form
+        onSubmit={handleSubmit(handleOnSubmit)}
+        className="flex flex-col gap-4"
+      >
+        <label htmlFor="formCheck" className="sr-only">
+          Please leave this field alone.
+        </label>
+        <input
+          type="checkbox"
+          id="formCheck"
+          name="formCheck"
+          className="hidden"
+          {...register("formCheck")}
+        />
 
-	return (
-		<div className="p-8 bg-white">
-			{title && (
-				<Heading level="span" levelStyle="h4" className="mb-4">
-					{title}
-				</Heading>
-			)}
-			<form
-				onSubmit={handleSubmit(handleOnSubmit)}
-				className="flex flex-col gap-4"
-			>
-				<label htmlFor="formCheck" className="sr-only">
-					Please leave this field alone.
-				</label>
-				<input
-					type="checkbox"
-					id="formCheck"
-					name="formCheck"
-					className="hidden"
-					{...register("formCheck")}
-				/>
-
-				<div className="">
-					<label htmlFor="name" className="block">
-						Name
-					</label>
-					<input
-						type="text"
-						name="name"
-						autoComplete="false"
-						defaultValue=""
-						className={`w-full focus:ring-4
+        <div className="">
+          <label htmlFor="name" className="block">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            autoComplete="false"
+            defaultValue=""
+            className={`w-full focus:ring-4
               ${
-								errors.name
-									? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-									: "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-							}`}
-						{...register("name", {
-							required: "Please enter your name",
-							maxLength: 80,
-						})}
-					/>
-					{errors.name && (
-						<div className="mt-1 text-red-600">
-							<small>{errors.name.message}</small>
-						</div>
-					)}
-				</div>
+                errors.name
+                  ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                  : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+              }`}
+            {...register("name", {
+              required: "Please enter your name",
+              maxLength: 80,
+            })}
+          />
+          {errors.name && (
+            <div className="mt-1 text-red-600">
+              <small>{errors.name.message}</small>
+            </div>
+          )}
+        </div>
 
-				<div className="">
-					<label htmlFor="email" className="block">
-						Email
-					</label>
-					<input
-						id="email_address"
-						type="email"
-						name="email"
-						autoComplete="false"
-						defaultValue=""
-						className={`w-full focus:ring-4 
+        <div className="">
+          <label htmlFor="email" className="block">
+            Email
+          </label>
+          <input
+            id="email_address"
+            type="email"
+            name="email"
+            autoComplete="false"
+            defaultValue=""
+            className={`w-full focus:ring-4 
               ${
-								errors.email
-									? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-									: "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-							}`}
-						{...register("email", {
-							required: "Please enter your email",
-							pattern: {
-								value: /^\S+@\S+$/i,
-								message: "Please enter a valid email",
-							},
-						})}
-					/>
-					{errors.email && (
-						<div className="mt-1 text-red-600">
-							<small>{errors.email.message}</small>
-						</div>
-					)}
-				</div>
+                errors.email
+                  ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                  : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+              }`}
+            {...register("email", {
+              required: "Please enter your email",
+              pattern: {
+                value: /^\S+@\S+$/i,
+                message: "Please enter a valid email",
+              },
+            })}
+          />
+          {errors.email && (
+            <div className="mt-1 text-red-600">
+              <small>{errors.email.message}</small>
+            </div>
+          )}
+        </div>
 
-				<div>
-					<label htmlFor="phone" className="block">
-						Phone
-					</label>
-					<input
-						id="phone"
-						type="tel"
-						name="phone"
-						autoComplete="false"
-						defaultValue=""
-						className={`w-full focus:ring-4 
+        <div>
+          <label htmlFor="phone" className="block">
+            Phone
+          </label>
+          <input
+            id="phone"
+            type="tel"
+            name="phone"
+            autoComplete="false"
+            defaultValue=""
+            className={`w-full focus:ring-4 
               ${
-								errors.phone
-									? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-									: "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-							}`}
-						{...register("phone", {
-							required: "Please enter your phone number",
-							pattern: {
-								value: /^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/,
-								message: "Please enter a valid phone number",
-							},
-						})}
-					/>
-					{errors.phone && (
-						<div className="mt-1 text-red-600">
-							<small>{errors.phone.message}</small>
-						</div>
-					)}
-				</div>
+                errors.phone
+                  ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                  : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+              }`}
+            {...register("phone", {
+              required: "Please enter your phone number",
+              pattern: {
+                value: /^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/,
+                message: "Please enter a valid phone number",
+              },
+            })}
+          />
+          {errors.phone && (
+            <div className="mt-1 text-red-600">
+              <small>{errors.phone.message}</small>
+            </div>
+          )}
+        </div>
 
-				<div>
-					<label htmlFor="project" className="block">
-						What type of project are you considering?
-					</label>
-					<select
-						id="project"
-						name="project"
-						autoComplete="false"
-						defaultValue=""
-						className={`w-full focus:ring-4
+        <div>
+          <label htmlFor="project" className="block">
+            What type of project are you considering?
+          </label>
+          <select
+            id="project"
+            name="project"
+            autoComplete="false"
+            defaultValue=""
+            className={`w-full focus:ring-4
             ${
-							errors.project
-								? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-								: "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-						}`}
-						{...register("project")}
-					>
-						<option value="" disabled>
-							---
-						</option>
-						<option value="kitchen remodel">Kitchen Remodel</option>
-						<option value="bathroom remodel">Bathroom Remodel</option>
-						<option value="other project">Other project</option>
-					</select>
-					{errors.project && (
-						<div className="mt-1 text-red-600">
-							<small>{errors.project.message}</small>
-						</div>
-					)}
-				</div>
+              errors.project
+                ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+            }`}
+            {...register("project")}
+          >
+            <option value="" disabled>
+              ---
+            </option>
+            <option value="kitchen remodel">Kitchen Remodel</option>
+            <option value="bathroom remodel">Bathroom Remodel</option>
+            <option value="other project">Other project</option>
+          </select>
+          {errors.project && (
+            <div className="mt-1 text-red-600">
+              <small>{errors.project.message}</small>
+            </div>
+          )}
+        </div>
 
-				<div className="mb-3">
-					<label htmlFor="message" className="block">
-						Message
-					</label>
-					<textarea
-						name="message"
-						defaultValue=""
-						className={`w-full focus:ring-4
+        <div className="mb-3">
+          <label htmlFor="message" className="block">
+            Message
+          </label>
+          <textarea
+            name="message"
+            defaultValue=""
+            className={`w-full focus:ring-4
               ${
-								errors.message
-									? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-									: "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-							}`}
-						{...register("message")}
-					/>
-					{errors.message && (
-						<div className="mt-1 text-red-600">
-							<small>{errors.message.message}</small>
-						</div>
-					)}
-				</div>
+                errors.message
+                  ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
+                  : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+              }`}
+            {...register("message")}
+          />
+          {errors.message && (
+            <div className="mt-1 text-red-600">
+              <small>{errors.message.message}</small>
+            </div>
+          )}
+        </div>
 
-				<HCaptcha
-					sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_KEY}
-					onVerify={(token, ekey) => onHCaptchaChange(token)}
-				/>
-				{errors["h-captcha-response"] && (
-					<div className="mt-1 text-red-600">
-						<small>{errors["h-captcha-response"].message}</small>
-					</div>
-				)}
+        <HCaptcha
+          sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_KEY}
+          onVerify={(token, ekey) => onHCaptchaChange(token)}
+        />
+        {errors["h-captcha-response"] && (
+          <div className="mt-1 text-red-600">
+            <small>{errors["h-captcha-response"].message}</small>
+          </div>
+        )}
 
-				<button
-					type="submit"
-					className={twMerge(
-						`bg-black text-white px-4 py-2 inline-flex justify-center rounded transition-colors hover:bg-gray-800 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-gray-200 disabled:cursor-not-allowed ${submitButtonClasses}`
-					)}
-					disabled={isSubmitting}
-				>
-					{isSubmitting ? (
-						<CircleNotch size={32} className="animate-spin" />
-					) : (
-						submitButtonText
-					)}
-				</button>
-			</form>
+        <button
+          type="submit"
+          className={twMerge(
+            `bg-black text-white px-4 py-2 inline-flex justify-center rounded transition-colors hover:bg-gray-800 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-gray-200 disabled:cursor-not-allowed ${submitButtonClasses}`
+          )}
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? (
+            <CircleNotch size={32} className="animate-spin" />
+          ) : (
+            submitButtonText
+          )}
+        </button>
+      </form>
 
-			{isSubmitSuccessful && isSuccess && (
-				<div className="my-4 text-sm text-center border p-4 border-green-600 text-green-600">
-					Thank you for your message! We will get back to you as soon as we can.
-				</div>
-			)}
-			{isSubmitSuccessful && !isSuccess && (
-				<div className="my-4 text-sm text-center border p-4 border-red-600 text-red-600">
-					{errors.form
-						? errors.form.message
-						: "Something went wrong. Please try again later."}
-				</div>
-			)}
-		</div>
-	);
+      {isSubmitSuccessful && isSuccess && (
+        <div className="my-4 text-sm text-center border p-4 border-green-600 text-green-600">
+          Thank you for your message! We will get back to you as soon as we can.
+        </div>
+      )}
+      {isSubmitSuccessful && !isSuccess && (
+        <div className="my-4 text-sm text-center border p-4 border-red-600 text-red-600">
+          {errors.form
+            ? errors.form.message
+            : "Something went wrong. Please try again later."}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ContactForm;
